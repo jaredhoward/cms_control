fcf95f0d66f105c34a656baf43ff9e790e2a8413
Jared Howard <jhoward@netmediagroup.com>
2010-07-21 08:40:26 -0600

    updated seeds to use nested attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
 
-page = CmsPage.create(:status => 'draft', :show_in_menu => true)
-page.cms_meta = CmsMeta.create(:url => 'home', 'title' => 'Main Pine', :description => 'default description', :keywords => 'default keywords', :metaable => page)
-page.cms_meta.current_cms_content = CmsContent.create(:contentable => page.cms_meta, :content => 'Hello World!')
-page.cms_meta.current_cms_content.save
-page.cms_meta.save
-page.save
+page = CmsPage.create({
+  :status => 'draft',
+  :show_in_menu => true,
+  :cms_meta_attributes => {
+    :url => 'home',
+    :title => 'Main Pine',
+    :description => 'default description',
+    :keywords => 'default keywords',
+    :cms_contents_attributes => [
+      {:content => 'Hello World!'}
+    ]
+  },
+})
