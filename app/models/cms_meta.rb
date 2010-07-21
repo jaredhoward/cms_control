e924b07d52bee6e894269a968fda298dc90ee910
Jared Howard <jhoward@netmediagroup.com>
2010-07-21 09:18:57 -0600

    module CommonContentable; show/edit field items for content title

--- app/models/cms_meta.rb
+++ app/models/cms_meta.rb
@@ -1,15 +1,10 @@
 class CmsMeta < ActiveRecord::Base
-  belongs_to :metaable, :polymorphic => true
-  belongs_to :current_cms_content, :class_name => 'CmsContent'
-  has_many :cms_contents, :as => :contentable, :dependent => :destroy
+  include CommonContentable
 
-  accepts_nested_attributes_for :cms_contents
+  belongs_to :metaable, :polymorphic => true
 
   validates_presence_of :metaable, :url, :title
   validates_uniqueness_of :url
-
-  before_validation_on_create :associate_cms_contents_contentable_to_self
-  # after_create :assign_current_cms_content
 
   def self.get_current(url)
     meta = self.find_by_url(url)
@@ -19,25 +14,6 @@ class CmsMeta < ActiveRecord::Base
     else
       return nil
     end
-  end
-
-protected
-
-  def assign_current_cms_content
-    if current_cms_content.nil?
-      unless self.cms_contents.empty?
-        self.current_cms_content_id = self.cms_contents.first.id
-        self.save!
-      end
-    end
-  end
-
-private
-
-  def associate_cms_contents_contentable_to_self
-    self.cms_contents.each do |cms_content|
-      cms_content.contentable = self if cms_content.contentable.nil?
-    end unless self.cms_contents.empty?
   end
 
 end
---
+++ lib/common_contentable.rb
@@ -0,0 +1,32 @@
+module CommonContentable
+
+  def self.included(base)
+    base.belongs_to :current_cms_content, :class_name => 'CmsContent'
+    base.has_many :cms_contents, :as => :contentable, :dependent => :destroy
+
+    base.accepts_nested_attributes_for :cms_contents
+
+    base.before_validation_on_create :associate_cms_contents_contentable_to_self
+    # base.after_create :assign_current_cms_content
+  end
+
+protected
+
+  def assign_current_cms_content
+    if current_cms_content.nil?
+      unless self.cms_contents.empty?
+        self.current_cms_content_id = self.cms_contents.first.id
+        self.save!
+      end
+    end
+  end
+
+private
+
+  def associate_cms_contents_contentable_to_self
+    self.cms_contents.each do |cms_content|
+      cms_content.contentable = self if cms_content.contentable.nil?
+    end unless self.cms_contents.empty?
+  end
+
+end
