990d7de2ac0587099df796f9561d1060981c6e3c
Mike Potter <mpotter@netmediagroup.com>
2010-07-28 08:39:37 -0600

    added tinymce

---
+++ public/javascripts/tiny_mce_init.js
@@ -0,0 +1,10 @@
+tinyMCE.init({
+  mode : "specific_textareas",
+  editor_selector : "mceEditor",
+  theme : "advanced",
+  theme_advanced_toolbar_location: "top",
+  plugins : "fullscreen, advimage, contextmenu, table",
+  theme_advanced_buttons3_add : "fullscreen, tablecontrols",
+  theme_advanced_resizing : true,
+  theme_advanced_statusbar_location : "bottom"
+});
