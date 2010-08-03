75e34272adeae2cf2795b04360b61da286663086
Mike Potter <mpotter@netmediagroup.com>
2010-08-03 09:32:47 -0600

    updated tinymce editor to resize horizontally

--- public/javascripts/tiny_mce_init.js
+++ public/javascripts/tiny_mce_init.js
@@ -3,8 +3,10 @@ tinyMCE.init({
   editor_selector : "mceEditor",
   theme : "advanced",
   theme_advanced_toolbar_location: "top",
+  theme_advanced_disable : 'help, code',
   plugins : "fullscreen, advimage, contextmenu, table",
   theme_advanced_buttons3_add : "fullscreen, tablecontrols",
-  theme_advanced_resizing : true,
-  theme_advanced_statusbar_location : "bottom"
+  theme_advanced_statusbar_location : "bottom",
+  width: '100%',
+  height: '100%'
 });
