e2f5bae978e0f55fa8cb8e6a704e7e8ce7a6e827
Mike Potter <mpotter@netmediagroup.com>
2010-08-09 08:07:16 -0600

    added get_title helper mainly for form_buttons partial; also changed titles on new and edit pages to use this new helper method

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,23 @@
 module AdminHelper
+
+  def get_title()
+    action = params[:action]
+
+    item = if params[:controller].include?('block') then 'Block'
+    elsif params[:controller].include?('page') then 'Page'
+    elsif params[:controller].include?('product') then 'Product'
+    elsif params[:controller].include?('file') then 'File'
+    else nil
+    end
+
+    action = if params[:action].include?('new') then 'Creating New'
+    elsif params[:action].include?('edit') then 'Editing'
+    elsif params[:action].include?('update') then 'Editing'
+    else nil
+    end
+    
+    return action + ' ' + item
+  end
+
+
 end
