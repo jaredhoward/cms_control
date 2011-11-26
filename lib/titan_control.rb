ea4ad503d236be551b7070ccbfacc8608d35e2ec
Jared Howard <jared@howardpants.com>
2011-11-26 00:12:04 -0700

    change require libraries for ModelCommonality

--- lib/titan_control.rb
+++ lib/titan_control.rb
@@ -1,9 +1,6 @@
-require "titan_control/engine"
 require "titan_control/model_commonality"
-require "titan_control/model_commonality/contentable"
-require "titan_control/model_commonality/metaable"
-require "titan_control/model_commonality/route_reload"
 
+require "titan_control/engine"
 require 'titan_control/railtie.rb' if defined?(Rails)
 
 module TitanControl
--- lib/titan_control/model_commonality.rb
+++ lib/titan_control/model_commonality.rb
@@ -4,3 +4,7 @@ module TitanControl
   module ModelCommonality
   end
 end
+
+require "titan_control/model_commonality/contentable"
+require "titan_control/model_commonality/metaable"
+require "titan_control/model_commonality/route_reload"
